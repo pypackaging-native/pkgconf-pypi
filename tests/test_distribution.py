a13d6e5ae26d78a5035ec781c8f5281f4d9805cc
Filipe Laíns <lains@riseup.net>
2024-03-28 19:01:53 +0000

    tests: add tests for the Python module
    Signed-off-by: Filipe Laíns <lains@riseup.net>

---
+++ tests/test_distribution.py
@@ -0,0 +1,17 @@
+import os
+import zipfile
+
+
+def test_single_pkgconfig_in_wheel_contents(self_wheel):
+    """pkgconf.get_executable() makes an assumption we are only installing one file named 'pkgconf'"""
+    if os.name == 'posix':
+        executable_name = 'pkgconf'
+    else:
+        raise NotImplementedError
+
+    wheel = zipfile.ZipFile(self_wheel)
+    pkgconf_entries = [
+        entry for entry in wheel.namelist()
+        if entry.split('/')[-1] == executable_name
+    ]
+    assert len(pkgconf_entries) == 1
