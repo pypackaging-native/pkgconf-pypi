22c3a6a841e0d8792359b287c4f05e994f5c1507
Filipe Laíns <lains@riseup.net>
2024-03-30 03:09:33 +0000

    py: place the pkgconf executable in the Python package
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- src/pkgconf/__init__.py
+++ src/pkgconf/__init__.py
@@ -6,34 +6,13 @@ import pathlib
 from collections.abc import Sequence
 
 
-_distribution = importlib.metadata.distribution(__package__)
-_distribution_name = f'{_distribution.name}-{_distribution.version}'
-
-
-def _iter_records(path: importlib.metadata.SimplePath) -> pathlib.Path:
-    for line in path.read_text().splitlines():
-        entry = line.split(',')[0]
-        abspath = os.path.abspath(os.path.join(path, '..', '..', entry))
-        yield pathlib.Path(abspath)
-
-
 def get_executable() -> pathlib.Path:
     """Get the pkgconf executable."""
     if os.name == 'posix':
         executable_name = 'pkgconf'
     else:
         raise NotImplementedError
-    # Try looking in .dist-info, in case .data/scripts got installed to the scripts location
-    record_file = _distribution.locate_file(f'{_distribution_name}.dist-info/RECORD')
-    if record_file.exists():
-        for record in _iter_records(record_file):
-            if record.name == executable_name:
-                return record
-    # Try looking in .data/scripts, in case it was kept in-place
-    path = _distribution.locate_file(f'{_distribution_name}.data/scripts/{executable_name}')
-    if path.exists():
-        return pathlib.Path(path)
-    raise ValueError('Could not find the pkgconf executable.')
+    return pathlib.Path(importlib.resources.files('pkgconf') / '.bin' / executable_name)
 
 
 def get_pkg_config_path() -> Sequence[str]:
