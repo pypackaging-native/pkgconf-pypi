95eebb0e8419a16c4890e950817c5ea5409f22ca
Filipe Laíns <lains@riseup.net>
2024-04-18 01:37:14 +0100

    py: add pkgconf.diagnose module
    Signed-off-by: Filipe Laíns <lains@riseup.net>

---
+++ src/pkgconf/diagnose.py
@@ -0,0 +1,25 @@
+import importlib.metadata
+import importlib.resources
+import os
+import pkgconf
+
+
+def report() -> None:
+    print(f'pkgconf executable: {pkgconf.get_executable()}')
+
+    entrypoints = importlib.metadata.entry_points(group='pkg-config')
+    print(f'entrypoints:')
+    for entry in entrypoints:
+        print(f'  {entry.name}:')
+        print(f'    value: {entry.value}')
+        print(f'    path: {os.fspath(importlib.resources.files(entry.value))}')
+
+    print(f'PKG_CONFIG_PATH: {os.pathsep.join(pkgconf.get_pkg_config_path())}')
+
+
+if __name__ == '__main__':
+    try:
+        report()
+    except (KeyboardInterrupt, BrokenPipeError):
+        pass
+
