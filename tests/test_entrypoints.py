feafa3a28c33d864fc7cea05f6d3094a4bc5d1e5
Filipe Laíns <lains@riseup.net>
2024-04-23 11:29:34 +0100

    py: add test for the system pkgconf fallback
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_entrypoints.py
+++ tests/test_entrypoints.py
@@ -1,8 +1,11 @@
 import os
 import shutil
+import sys
 import subprocess
 
 import pytest
+
+import pkgconf
 
 
 @pytest.mark.parametrize('name', ['pkgconf', 'pkg-config'])
@@ -11,3 +14,18 @@ def test_entries(env, name):
     path = shutil.which(name, path=env.scripts)
     help_text = subprocess.check_output([os.fspath(path), '--help'])
     assert help_text.decode().startswith('usage: pkgconf')
+
+
+def test_fallback(mocker, monkeypatch):
+    """Test that we fallback to the system pkgconf if ours fails."""
+    mocker.patch('pkgconf.run_pkgconf', side_effect=subprocess.CalledProcessError(1, '(cmd)'))
+    mocker.patch('subprocess.run')
+
+    mocker.patch('shutil.which', return_value='(pkgconf-path)')
+
+    args = ['--libs', 'py-test-inexistent']
+    monkeypatch.setattr(sys, 'argv', ['(argv0)', *args])
+
+    pkgconf._entrypoint()
+
+    subprocess.run.assert_called_with(['(pkgconf-path)', *args])
