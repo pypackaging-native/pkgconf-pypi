199a4b061bcd94e5f24923ff9b521e2b8625efa9
Filipe Laíns <lains@riseup.net>
2024-05-28 18:45:25 +0100

    examples: add header-only-library
    Signed-off-by: Filipe Laíns <lains@riseup.net>

---
+++ tests/test_examples.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+
+
+def test_header_only_library(env, tmp_path, examples, data):
+    env.install_from_path(examples / 'header-only-library', from_sdist=False)
+
+    src = os.fspath(data / 'needs-example-lib.c')
+    bin = os.fspath(tmp_path / 'needs-example-lib')
+
+    pkgconf = os.path.join(env.scheme['scripts'], 'pkgconf')
+    cflags = subprocess.check_output([pkgconf, '--cflags', 'example']).decode().split()
+    subprocess.check_call(['cc', '-o', bin, src, *cflags])
+    out = subprocess.check_output([bin])
+
+    assert out == b'bar'
