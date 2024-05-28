59023c2f939931741e949cae363e0351256519c3
Filipe Laíns <lains@riseup.net>
2024-05-28 17:21:41 +0100

    py: fix calculating PKG_CONFIG_PATH for namespace packages
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_python_module.py
+++ tests/test_python_module.py
@@ -19,6 +19,16 @@ def test_pkg_config_path(env, packages):
     assert list(path) == [os.path.join(env.scheme['purelib'], 'example', 'pkgconf')]
 
 
+def test_pkg_config_path_namespace(env, packages):
+    path = list(env.introspectable.call('pkgconf.get_pkg_config_path'))
+    assert len(path) == 0
+
+    env.install_from_path(packages / 'namespace', from_sdist=False)
+
+    path = list(env.introspectable.call('pkgconf.get_pkg_config_path'))
+    assert list(path) == [os.path.join(env.scheme['purelib'], 'example')]
+
+
 def test_run_pkgconfig(env):
     output = env.introspectable.call('pkgconf.run_pkgconf', '--help', capture_output=True)
     assert output.stdout.decode().startswith('usage: pkgconf')
