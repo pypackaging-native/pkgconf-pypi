e3d703e974c6f4816303d491b7956277ed2dbc73
Filipe Laíns <lains@riseup.net>
2024-05-30 11:10:53 +0100

    tests: ignore casing in test_single_pkgconfig_in_wheel_contents
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -7,10 +7,10 @@ def test_single_pkgconfig_in_wheel_contents(self_wheel):
     if os.name == 'posix':
         executable_name = 'pkgconf'
     elif os.name == 'nt':
-        executable_name = 'pkgconf.EXE'
+        executable_name = 'pkgconf.exe'
     else:
         raise NotImplementedError
 
     wheel = zipfile.ZipFile(self_wheel)
-    pkgconf_entries = [entry for entry in wheel.namelist() if entry.split('/')[-1] == executable_name]
+    pkgconf_entries = [entry for entry in wheel.namelist() if entry.split('/')[-1].lower() == executable_name]
     assert len(pkgconf_entries) == 1
