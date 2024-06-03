47f2ecdc966a10ccac7df17e19b7c2cda169373f
Filipe Laíns <lains@riseup.net>
2024-06-04 00:12:31 +0100

    py: diagnose: print paths in a nicer way
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- src/pkgconf/diagnose.py
+++ src/pkgconf/diagnose.py
@@ -10,9 +10,10 @@ def report() -> None:
     entrypoints = importlib.metadata.entry_points(group='pkg_config')
     print('entrypoints:')
     for entry in entrypoints:
+        paths_str = ', '.join(pkgconf._get_module_paths(entry.value))
         print(f'  {entry.name}:')
         print(f'    value: {entry.value}')
-        print(f'    paths: {pkgconf._get_module_paths(entry.value)}')
+        print(f'    paths: {paths_str}')
 
     print(f'PKG_CONFIG_PATH: {os.pathsep.join(pkgconf.get_pkg_config_path())}')
 
