3afb1406b6e499ce5fe69ef1dd1ca4be0ffa0889
Filipe Laíns <lains@riseup.net>
2025-01-13 00:54:35 +0000

    test: fix test_header_only_library
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -8,8 +8,7 @@ def test_header_only_library(env, tmp_path, examples, data):
     src = os.fspath(data / 'needs-example-lib.c')
     bin = os.fspath(tmp_path / 'needs-example-lib')
 
-    pkgconf = os.path.join(env.scheme['scripts'], 'pkgconf')
-    cflags = subprocess.check_output([pkgconf, '--cflags', 'example']).decode().split()
+    cflags = env.run_interpreter('-m', 'pkgconf', '--cflags', 'example').decode().split()
     subprocess.check_call(['gcc', '-o', bin, src, *cflags])
     out = subprocess.check_output([bin])
 
